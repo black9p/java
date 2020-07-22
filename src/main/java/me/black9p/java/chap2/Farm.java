9f7b4b69d0f4f3c190cb87d12bd5f9cffbb6e8bf
black9p <black9p@naver.com>
2020-07-22 23:02:16 +0900

    Filter apples by color

--- src/main/java/me/black9p/java/chap2/Farm.java
+++ src/main/java/me/black9p/java/chap2/Farm.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class Farm {
 
+    /**
+     * 1. 녹색 사과 필터링
+     * @param inventory
+     * @return
+     */
     public static List<Apple> filterGreenApples(List<Apple> inventory) {
         List<Apple> result = new ArrayList<>();
 
@@ -15,4 +20,23 @@ public class Farm {
         }
         return result;
     }
+
+    /**
+     * 2. 색을 파라미터화
+     * @param inventory
+     * @param color
+     * @return
+     */
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, String color) {
+        List<Apple> result = new ArrayList<>();
+
+        for(Apple apple : inventory) {
+            if (color.equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/me/black9p/java/chap2/FarmTest.java
+++ src/test/java/me/black9p/java/chap2/FarmTest.java
@@ -3,7 +3,6 @@ package me.black9p.java.chap2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,9 +13,19 @@ class FarmTest {
     public void test_filterGreenApples() {
         List<Apple> inventory = Arrays.asList(new Apple(FarmConstants.GREEN_COLOR), new Apple(FarmConstants.BLUE_COLOR));
 
-        List<Apple> expected = Collections.singletonList(new Apple(FarmConstants.GREEN_COLOR));
+        int expected = 1;
         List<Apple> actual = Farm.filterGreenApples(inventory);
 
-        assertEquals(expected.size(), actual.size());
+        assertEquals(expected, actual.size());
+    }
+
+    @Test
+    public void test_filterApplesByColor() {
+        List<Apple> inventory = Arrays.asList(new Apple(FarmConstants.BLUE_COLOR), new Apple(FarmConstants.BLUE_COLOR), new Apple(FarmConstants.RED_COLOR));
+
+        int expected = 2;
+        List<Apple> actual = Farm.filterApplesByColor(inventory, FarmConstants.BLUE_COLOR);
+
+        assertEquals(expected, actual.size());
     }
 }
